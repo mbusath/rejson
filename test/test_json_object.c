10b355b1be8ca758bcb442857fe1aa8f48d7ff87
Itamar Haber <itamar@redislabs.com>
2016-06-23 05:53:10 -0700

    Changes formatting (ident:4)

--- test/test_json_object.c
+++ test/test_json_object.c
@@ -7,7 +7,7 @@
 #define _JSTR(e) "\"" #e "\""
 
 MU_TEST(testJSONObject) {
-  const char SampleJSON[] =
+    const char SampleJSON[] =
         "{"
             _JSTR(foo) ": {"
                 _JSTR(bar) ": ["
@@ -20,27 +20,24 @@ MU_TEST(testJSONObject) {
            "}"
         "}";
 
-  Node *n1 = CreateNodeFromJSON(SampleJSON, strlen(SampleJSON), NULL);
-  mu_check(n1);
-  mu_check(N_DICT == n1->type);
-  mu_check(1 == n1->value.dictval.len);
+    Node *n1 = CreateNodeFromJSON(SampleJSON, strlen(SampleJSON), NULL);
+    mu_check(n1);
+    mu_check(N_DICT == n1->type);
+    mu_check(1 == n1->value.dictval.len);
 
-  Node *n2;
-  mu_check(OBJ_ERR == Node_DictGet(n1, "f00", &n2));
-  mu_check(OBJ_ERR == Node_DictGet(n1, "bar", &n2));
-  mu_check(OBJ_ERR == Node_DictGet(n1, "baz", &n2));
-  mu_check(OBJ_OK == Node_DictGet(n1, "foo", &n2));
-  mu_check(N_DICT == n2->type);
-  mu_check(2 == n2->value.dictval.len);
+    Node *n2;
+    mu_check(OBJ_ERR == Node_DictGet(n1, "f00", &n2));
+    mu_check(OBJ_ERR == Node_DictGet(n1, "bar", &n2));
+    mu_check(OBJ_ERR == Node_DictGet(n1, "baz", &n2));
+    mu_check(OBJ_OK == Node_DictGet(n1, "foo", &n2));
+    mu_check(N_DICT == n2->type);
+    mu_check(2 == n2->value.dictval.len);
 }
 
-MU_TEST_SUITE(test_json_object) {
-
-  MU_RUN_TEST(testJSONObject);
-}
+MU_TEST_SUITE(test_json_object) { MU_RUN_TEST(testJSONObject); }
 
 int main(int argc, char *argv[]) {
-  MU_RUN_SUITE(test_json_object);
-  MU_REPORT();
-  return minunit_status;
+    MU_RUN_SUITE(test_json_object);
+    MU_REPORT();
+    return minunit_status;
 }
